7863a473f484ad7f5f318b4a4c184302ba043889
Joshua Ferge <josh.ferge@gmail.com>
2015-04-03 15:21:12 -0600

    fixed indentation, filenames added readme

---
+++ example.py
@@ -0,0 +1,21 @@
+import csv
+import fr
+
+
+fopen = open('info.txt','r')
+fname = 'info.txt'
+
+print fr.get_len(fname)
+print fr.get_line(fname,1),
+print fr.get_element(fname,2,0)
+
+print ("printing lines inbetween certain lines")
+print fr.iterate_certainlines(fname,0,2)
+
+
+def addOne(x):
+    return x+1
+
+print ("printing the array with the function applied")
+print fr.func_nextlines(fname,addOne,0,2)
+
